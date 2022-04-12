2cc6b3d1544c19511ed228da967fa333f699aba9
Juandavidmendi <92585848+Juandavidmendi@users.noreply.github.com>
2022-04-12 17:37:21 -0500

    Aprendi promedios

--- README.md
+++ README.md
@@ -13,3 +13,9 @@
 -Segundo paso: implementar las formulas en JavaScript 
 -Tercer paso: crear funciones
 -Cuarto paso: Integrar JavaScript en HTML
+
+##Taller #3 Promedia, media y mediana
+-Primer paso;: fefinir las formulas
+-Segundo paso: implementar las formulas en JavaScript 
+-Tercer paso: crear funciones
+-Cuarto paso: Integrar JavaScript en HTML
--- js/descuentos.js
+++ js/descuentos.js
@@ -1,6 +1,10 @@
 //const precioOriginal  = 120;
 //const descuento = 18;
-
+const cupones = [
+    "un_producto",
+    "2_o_mas_de_dos_productos",
+    "3_o_mas_de_dos_productos",
+];
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajeConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajeConDescuento)/ 100;
@@ -8,8 +12,33 @@ function calcularPrecioConDescuento(precio, descuento){
     return precioConDescuento;
 }
 
-function onClickButtonPriceDiscount(){
-    document.getElementById("inputPrice");
+function unClicYCalcula(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+      
+    //const inputDiscount = document.getElementById("inputDiscount");
+    //const discountValue = inputDiscount.value;
     
-    document.getElementById("inputPrice");
+    const inputCupon = document.getElementById("InputCoupon");
+    const cuponValue = inputCupon.value;
+
+    let descuento;
+
+    switch(cuponValue){
+        case cupones[0]:
+            descuento = 15;
+            break ;
+        case cupones[1]:
+            descuento = 30; 
+            break;
+        case cupones[2]:
+            descuento = 25;  
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    //insertar texto en HTML
+    const resultadoP = document.getElementById("resultadoP");
+    resultadoP.innerText = "El precio con descuento es: $"+precioConDescuento;
 }
+
